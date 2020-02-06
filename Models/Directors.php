625cac458178be7ce7941bd483c9269a3f13c4e2
joansor <sor.joan@gmail.com>
2020-02-06 20:45:04 +0100

    ajout d'un show.html.twig

--- Models/Directors.php
+++ Models/Directors.php
@@ -7,7 +7,7 @@ class Directors extends Model
         $this->pdo = parent::getPdo();
     }
     public function getOneExemple($id) {
-    $req = $this->pdo->prepare('SELECT realiser.*, artiste.id_a FROM realiser, artiste WHERE artistes.id_a ="'.$id.'"');
+    $req = $this->pdo->prepare('SELECT realiser.*, artistes.*, films.* FROM realiser, artistes, films WHERE artistes.id_a ='.$id.' AND artistes.id_a = realiser.Artistes_id_a AND realiser.Films_id_f = films.id_f');
         $req->execute([$id]);
         return $req->fetch();
     }
--- index.php
+++ index.php
@@ -6,17 +6,19 @@ $router = new Router($_GET['url']);
 
 
 //liste de nos routes
-//routes artistes
-$router->get('/actors/show/:id', "Actors.show"); // Actors.show => Actors = ActorsController.php ; show = function show
+//routes des id
+$router->get('/actors/show/:id', "Actors.show"); // Actors.show => Actors = ActorsController.php ; show = function show(méthod)
 $router->get('/films/show/:id', "Home.show");
+$router->get('/directors/show/:id', "Directors.show");
+
+//route des lists
 $router->get("/actors", "Actors.list");
 $router->get("/directors", "Directors.list");
-//route films et page home
-
 $router->get("/admin", "Admin.form");
-$router->get("/", "Home.list");
-//routes genres
 $router->get("/genres", "Genres.cloud");
+
+//routes home
+$router->get("/", "Home.list");
 
 
 $router->run();
