ccf0a3367456a83d49411e8056890af974488014
joansor <sor.joan@gmail.com>
2020-02-06 09:50:39 +0100

    correction des bugs

--- index.php
+++ index.php
@@ -8,13 +8,13 @@ $router = new Router($_GET['url']);
 //liste de nos routes
 //routes actors
 $router->get('/actors/show/:id', "Actors.show"); // Actors.show => Actors = ActorsController.php ; show = function show
-$router->get("/", "Home.show");
 $router->get("/actors", "Actors.list");
 //route films et page home
 $router->get('/films/:id', "Home.films");
+$router->get("/admin", "Admin.form");
 $router->get("/", "Home.list");
 //routes genres
 $router->get("/genres", "Genres.show");
-$router->get("/admin", "Admin.form");
+
 
 $router->run();
