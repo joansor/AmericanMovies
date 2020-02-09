a9fc2c03340fd8553b621d28f0f2eaa272df6f00
joansor <sor.joan@gmail.com>
2020-02-09 15:27:33 +0100

    travail sur l'admin

--- Controllers/AdminController.php
+++ Controllers/AdminController.php
@@ -15,22 +15,53 @@ class AdminController extends Controller
         echo $template->render();
     }
 
+    // debut de fonction pour se connecter
     public function log()
     {
+        if ($_POST["uname"] === "admin" && $_POST["psw"] ==="admin" ) {
+            session_start();
+            $_SESSION['type_user'] = "admin";
+            header('Location: http://localhost/AmericanMovies/admin/dashboard');
+        }
         if ($userInfo = $this->model->connect($_POST["uname"])) {
             //Le username existe dans la BDD
-            if (password_verify($_POST["psw"], $userInfo["password"])) {
+
+            if (password_verify($_POST["psw"], $userInfo["password"])){
                 //Le username et le mot de passe correspondent, tout est ok
+                // session_start();
+                // $_SESSION['type'] = "user";
+                header('Location: http://localhost/AmericanMovies/');
                 echo "Bienvenue";
-               if($_POST["uname"] === "admin"){
-                    header('Location: http://www.google.com/');
-                }
+                
             } else {
                 //Le username existe mais le mot de passe est faux
                 echo "Mot de passe faux";
             }
         } else {
             //Le username n'existe pas dans la BDD
+            echo"**************************************";
+        }
+    } //  fin function pour se connecter
+
+
+    //debut de function enregistrer le donnée du form
+    public function register()
+    {
+        
+        if ($this->model->connect($_POST["register"])) {
+
+            if (isset($_POST["pass"]) && (isset($_POST["username"]))) {
+                session_start();
+                $_SESSION['type_user'];
+                var_dump($_POST["pass"]);
+                var_dump($_POST["username"]);
+                echo "enregister";
+               
+                header('Location: http://localhost/AmericanMovies/admin/dashboard');
+            } else {
+
+                echo "Veuillez remplir les champs";
+            }
         }
     }
 }
--- index.php
+++ index.php
@@ -10,6 +10,9 @@ $router->get('/films/show/:id', "Home.show");
 $router->get("/genres/list/:id", "Genres.list");
 $router->get('/directors/show/:id', "Directors.show");
 $router->post('/admin/log', 'Admin.log');
+$router->get('/admin/dashboard', 'Admin.log');
+$router->post('/admin/dashboard', 'Admin.register');
+
 
 //premier niveau
 $router->get("/actors", "Actors.list");
