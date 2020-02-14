cc5c096c8e59d63d07d7dd6505c8eebad7951a9e
HelixFix <jerome.rinner@gmail.com>
2020-02-14 20:51:04 +0100

    *

---
+++ Controllers/UsersController.php
@@ -0,0 +1,131 @@
+<?php
+
+class UsersController extends Controller
+{
+	###############################################################################
+	#### CONSTRUCTEUR NOUVEL UTILISATEUR ##########################################
+	###############################################################################
+
+	public function __construct()
+	{
+		parent::__construct(); // Parent dans Controller.php
+		$this->model = new Users(); // Nouvel Object : Films
+	}
+
+	###############################################################################
+	#### FONCTION MON COMPTE - partie a construire ################################
+	###############################################################################
+
+	public function my_account()
+	{
+		global $admin, $user; // Superglobales
+
+		$pageTwig = 'users/my_account.html.twig'; // Chemin de la View
+		$template = $this->twig->load($pageTwig); // chargement de la View
+		echo $template->render(["admin" => $admin, "user" => $user]); // Affiche la view et passe les données en paramêtres
+	}
+
+	#################################################################################
+	#### FONCTION INDEX - CHOIX DE SE CONNECTER OU DE SE CREER UN COMPTE ############
+	#################################################################################
+
+	public function index()
+	{
+		global $admin, $user; // Superglobales
+
+		$pageTwig = 'users/index.html.twig'; // Chemin de la View
+		$template = $this->twig->load($pageTwig); // chargement de la View
+		echo $template->render(["admin" => $admin, "user" => $user]); // Affiche la view et passe les données en paramêtres
+	}
+
+	##################################################################################
+	#### FONCTION QUI TRAITE LES DONNEES SAISIENT LORS D'UNE NOUVELLE INSCRIPTION ####
+	##################################################################################
+
+	public function register()
+	{
+		$pageTwig = 'traitement.html.twig'; // Chemin de la View
+		$template = $this->twig->load($pageTwig); // chargement de la View
+
+		if (isset($_POST["pass"]) && (isset($_POST["username"]) && (isset($_POST["email"]))))  // Si un pseudo et un mot de passe ont bien été saisi
+		{
+			$userverif = $this->model->getVerifUser($_POST["username"]); // Vérifie dans la bdd si le pseudo existe déjà
+			$mailverif  = $this->model->getVerifEmail($_POST["email"]); // Vérifie dans la bdd si l'email existe déjà
+			
+			if($userverif) // le pseudo existe deja
+			{
+				$message = "Ce pseudo est deja pris"; // Message a afficher
+			}
+
+			if($mailverif) // Le mail existe déjà
+			{
+				$message = "Ce mail est deja pris"; // Message a afficher
+			}
+            else // Pseudo et Mail dispo, donc on peut enregistrer le nouvel utilisateur
+            {
+				$insertCompte = $this->model->registre($_POST["pass"], $_POST["username"], $_POST["email"]); // Insertion de l'utilisateur dans la bdd
+				$message = "Votre compte a bien été créé"; // Message à afficher
+			}
+			
+			redirect("../Users", 5); // Redirection vers page users après 5s
+		}
+		else 
+		{
+			$message = "Veuillez remplir les champs. Patientez, vous allez être redirigé"; // Message à afficher
+			redirect("javascript:history.back()", 5); // Redirection vers page users
+		}
+
+		echo $template->render(["message" => $message]); // Affiche la view et passe les données en paramêtres
+	}
+
+	###############################################################################
+	#### FONCTION QUI TRAITE LES DONNEES SAISIENT DANS FORMULAIRE DE CONNEXION ####
+	###############################################################################
+
+	public function traitement_connexion()
+	{
+		$pageTwig = 'users/traitement.html.twig'; // Chemin vers la View
+		$template = $this->twig->load($pageTwig); // Chargement de la view
+
+		$userInfo = $this->model->connect($_POST["uname"]); // Vérifie dans la bdd si le pseudo existe
+
+		if ($userInfo) // Si $userInfo retoune une valeur, alors l'utilisateur existe dans la bdd
+		{
+			if (password_verify($_POST["psw"], $userInfo["password"])) // Si le password saisi = password bdd, alors la connexion est réussi
+			{
+				$message = "Connexion En cours ..."; // Message à afficher)
+				$_SESSION["connected"]=true; // Création d'une variable connected pour resté connecté sur toutes les pages
+				$_SESSION["user"] = ["userid" => $userInfo['id_u'], "username" => ucwords(strtolower($userInfo['username'])), "usertype" => $userInfo['type_user'], "usermail" => $userInfo['email']];
+				redirect("../films", 1); // Redirection vers page films après 1s
+			} 
+			else // le mot de passe est faux
+			{
+				$message = "Mot de passe ne correspond pas. Merci de patientez, vous allez être redirigé"; // Message à afficher
+				$_SESSION["connected"] = false; // Variable connected false = l'utilisateur reste non connecté
+				$_SESSION["user"] = ""; // Variable utilisateur = rien
+				redirect("javascript:history.back()", 5); // Redirection vers la page du formulaire de connexion après 5s
+			}
+		} 
+		else // Sinon, l'username saisi n'existe pas dans la bdd
+		{
+			$message = "L'utilisateur « ". $_POST["psw"] ." » n'existe pas dans notre base de données"; // Message à afficher
+			$_SESSION["connected"] = false; // Variable connected false = l'utilisateur reste non connecté
+			$_SESSION["user"] = ""; // Variable utilisateur = rien
+			redirect("javascript:history.back()", 5); // Redirection vers la page du formulaire de connexion après 5s
+		}
+
+		echo $template->render(["message" => $message]); // Affiche la view et passe les données en paramêtres
+	}
+
+	#################################################################################
+	#### FONCTION LOGOUT - DECONNEXION DE L'UTILISATEUR #############################
+	#################################################################################
+
+	public function logout()
+	{
+		$_SESSION["connected"] = false; // Variable connected false = l'utilisateur n'est plus connecté
+		$_SESSION["user"] = ""; // Variable utilisateur = rien
+		session_destroy(); // Détruit la session
+		redirect("../Films", 0); // Redirection immédiate vers films
+	}
+}
---
+++ index.php
@@ -0,0 +1,99 @@
+<?php
+
+session_start();
+    require_once 'vendor/autoload.php';
+    include("functions.php");
+
+    setlocale(LC_TIME, 'fr_FR.utf8','fra'); // Pour mettre les dates en français
+
+    ################################################################
+    ##### SUPERGLOBALES ############################################
+    ################################################################
+
+	$superglobals = array($_SERVER, $_ENV, $_FILES, $_COOKIE, $_POST, $_GET);
+
+	foreach($superglobals as $superglobal)
+	{ 
+		foreach($superglobal as $key => $value) { if(!is_array($value)) { ${$key} = trim(rawurldecode($value)); /* echo "$key $value<br>"; */ }  else { ${$key} = $value; } }
+	}
+
+    global $admin, $logout;
+
+    ################################################################
+    ################################################################
+    ################################################################
+
+    if(!empty($_SESSION["user"]))
+    {
+        $user = $_SESSION["user"];
+
+        if($user["usertype"] == "admin") $admin = true ; else $admin = false;
+
+        // $user["userid"] = id de l'utilisateur  
+        // $user["username"] = pseudo de l'utilisateur
+        // $user["usertype"] = type d'utilisateur (admin/user)
+        // $user["usermail"] = email de l'utilisateur
+    } 
+    else 
+    {
+        $admin = false;
+    }
+
+    ################################################################
+    ################################################################
+    ################################################################
+
+    if(isset($_SERVER['REDIRECT_QUERY_STRING']))
+    {
+        // Récupère la première partie de l'url (artists/1)
+        $adresse = $_SERVER['REDIRECT_QUERY_STRING'];
+        $explode = explode("/", $adresse);
+        $section = $explode[0];
+        $section = str_replace("url=", "", $section);
+        $count = count($explode);
+        if($count > 1) $repertoire = $explode[1]; else $repertoire = "";
+    }
+
+    ################################################################
+    #### ROUTES ####################################################
+    ################################################################
+
+    if(isset($_GET['url']))
+    {
+        $router = new Router($_GET['url']);
+
+        $router->get("/artists/:categorie/show/:id", "Artists.show"); // Artists.show => Artists = ArtistsController.php ; show = function show(méthod)
+        $router->get('/artists/add', "Artists.add");
+        $router->get('/artists/jouer', "Artists.jouer");
+        $router->post('/artists/insert', "Artists.insert");
+        $router->get('/artists/edition/:id', "Artists.edition");
+        $router->post('/artists/update/:id', "Artists.update");
+        $router->get('/artists/suppression/:id', "Artists.suppression");
+        $router->get("/artists/:categorie", "Artists.categorie");
+        $router->get("/artists", "Artists.index");
+
+        $router->get('/films/show/:id/:slug', "Films.show");
+        $router->get('/films/show/:id', "Films.show");
+        $router->get('/films/add', "Films.add");
+        $router->post('/films/insert', "Films.insert");
+        $router->post('/films/insert_commentaire', "Films.insert_commentaire");
+        $router->get('/films/edition/:id', "Films.edition");
+        $router->post('/films/update/:id', "Films.update");
+        $router->get('/films/delete_commentaire/:id', "Films.delete_commentaire");
+        $router->get('/films/suppression/:id', "Films.suppression");
+        $router->get("/films", "Films.listing");
+
+        $router->get("/genres/list/:id", "Genres.index");
+        $router->get("/genres", "Genres.cloud");
+
+        $router->get('/users/my_account', 'Users.my_account');
+        $router->post('/users/traitement_connexion', 'Users.traitement_connexion');
+        $router->get('/users/logout', 'Users.logout');
+        $router->post('/users/register', 'Users.register');
+        $router->get("/users", "Users.index");
+
+        $router->get("/", "Films.listing");
+
+        $router->run();
+    }
+
